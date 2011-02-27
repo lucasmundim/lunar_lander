c58535c0cbe4230aead7ce8f6698971f2a1926d2
Lucas Mundim <lucas.mundim@corp.globo.com>
2011-02-27 19:57:15 -0300

    Adding main jet particles

--- lib/lunar_lander/game.rb
+++ lib/lunar_lander/game.rb
@@ -9,5 +9,14 @@ module LunarLander
       @player = Player.create({ :x => width/2, :y => height/2})
       @player.input = {:holding_left => :rotate_left, :holding_right => :rotate_right, :holding_up => :thrust}
     end
+    
+    def update
+      super
+      game_objects.destroy_if { |object| 
+        if object.kind_of? Chingu::Particle
+          object.outside_window? || object.color.alpha == 0
+        end
+      }
+    end
   end
 end
--- lib/lunar_lander/game_objects/player.rb
+++ lib/lunar_lander/game_objects/player.rb
@@ -9,6 +9,8 @@ module LunarLander
     def setup
       self.acceleration_y = 0.01
       self.velocity_y = 1
+      
+      @particle_animation = Chingu::Animation.new(:file => "particle.png", :size => [32,32])
     end
 
     def rotate_left
@@ -22,6 +24,20 @@ module LunarLander
     def thrust
       self.velocity_x += Gosu::offset_x(@angle, 0.02)
       self.velocity_y += Gosu::offset_y(@angle, 0.02)
+
+      Chingu::Particle.create( 
+        :x => @x - Gosu::offset_x(@angle, 20), 
+        :y => @y - Gosu::offset_y(@angle, 20), 
+        :animation => @particle_animation,
+        :scale_rate => -0.03, 
+        :fade_rate => -35, 
+        :rotation_rate => +1,
+        :mode => :default
+      )
+      Chingu::Particle.each { |particle| 
+        particle.y -= Gosu::offset_y(@angle, 10 + rand(4))
+        particle.x -= Gosu::offset_x(@angle, 10 + rand(4))
+      }
     end
   end
 end
