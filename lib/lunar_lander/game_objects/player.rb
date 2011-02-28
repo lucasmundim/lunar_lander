a9679af584114d80e5231d90dd3bfe82f2470b69
Lucas Mundim <lucas.mundim@corp.globo.com>
2011-02-28 02:33:45 -0300

    Side jets

--- lib/lunar_lander/game_objects/player.rb
+++ lib/lunar_lander/game_objects/player.rb
@@ -15,10 +15,40 @@ module LunarLander
 
     def rotate_left
       @angle -= 0.5
+      
+      Chingu::Particle.create( 
+        :x => @x + Gosu::offset_x(@angle + 60, 20),
+        :y => @y + Gosu::offset_y(@angle + 60, 20),
+        :animation => @particle_animation,
+        :scale_rate => -0.03, 
+        :fade_rate => -35, 
+        :rotation_rate => +1,
+        :mode => :default,
+        :factor => 0.5
+      )
+      Chingu::Particle.each { |particle| 
+        particle.y -= Gosu::offset_y(@angle-90, 2)
+        particle.x -= Gosu::offset_x(@angle-90, 2)
+      }
     end
 
     def rotate_right
       @angle += 0.5
+      
+      Chingu::Particle.create( 
+        :x => @x + Gosu::offset_x(@angle - 60, 20), 
+        :y => @y + Gosu::offset_y(@angle - 60, 20), 
+        :animation => @particle_animation,
+        :scale_rate => -0.03, 
+        :fade_rate => -35, 
+        :rotation_rate => +1,
+        :mode => :default,
+        :factor => 0.5
+      )
+      Chingu::Particle.each { |particle| 
+        particle.y -= Gosu::offset_y(@angle+90, 2)
+        particle.x -= Gosu::offset_x(@angle+90, 2)
+      }
     end
     
     def thrust
