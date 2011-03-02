334f9d84201b911d56a4c976f60eeef52889019c
Lucas Mundim <lucas.mundim@corp.globo.com>
2011-03-02 02:23:28 -0300

    Adding hud.

--- lib/lunar_lander/game_states/play.rb
+++ lib/lunar_lander/game_states/play.rb
@@ -8,16 +8,41 @@ module LunarLander
       
       @background = Gosu::Image["moon.png"]
       @surface = Chingu::Rect.new(0, $window.height-50, 800, 50)
+      
+      setup_hud
     end
     
     def update
       super
+      
       game_objects.destroy_if { |object| 
         if object.kind_of? Chingu::Particle
           object.outside_window? || object.color.alpha == 0
         end
       }
       
+      test_colision
+      
+       update_hud
+    end
+    
+    def setup_hud
+      @velocity_x_text = Chingu::Text.create("Velocidade Lateral: 0", :x => 10, :y => 10, :zorder => 55, :size=>20)
+      @velocity_y_text = Chingu::Text.create("Velocidade Vertical: 0", :x => 10, :y => 30, :zorder => 55, :size=>20)
+      @angle_text = Chingu::Text.create("Ângulo: 0", :x => 10, :y => 50, :zorder => 55, :size=>20)
+      @fuel_text = Chingu::Text.create("Combustível: #{@player.fuel}", :x => 10, :y => 70, :zorder => 55, :size=>20)
+    end
+    
+    def update_hud
+      if @player
+         @velocity_x_text.text = "Velocidade Lateral: #{(@player.velocity_x * 10).ceil.abs}"
+         @velocity_y_text.text = "Velocidade Vertical: #{(@player.velocity_y * 10).ceil * -1}"
+         @angle_text.text = "Ângulo: #{@player.angle}"
+         @fuel_text.text = "Combustível: #{@player.fuel.ceil}"
+      end
+    end
+    
+    def test_colision
       if @player.bounding_box.collide_rect?(@surface)
         if @player.should_die?
           @player.die
@@ -27,9 +52,11 @@ module LunarLander
       end
     end
     
+    
     def draw
       super
       @background.draw(0,0,0)
+      
     end
   end
 end
