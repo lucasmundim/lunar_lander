48258d8ce1506b4d5ff8da1f9a49f5c72e62d7ef
Lucas Mundim <lucas.mundim@corp.globo.com>
2011-03-02 02:58:38 -0300

    Removing bundler runtime dependency

--- lib/lunar_lander.rb
+++ lib/lunar_lander.rb
@@ -1,22 +1,3 @@
-begin
-  require "bundler"
-rescue LoadError
-  raise "Could not load the bundler gem. Install it with `gem install bundler`."
-end
-
-if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
-  raise RuntimeError, "Your bundler version is too old." +
-   "Run `gem install bundler` to upgrade."
-end
-
-begin
-  Bundler.setup(:default)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-
 require 'chingu'
 require 'lunar_lander/game'
 require 'lunar_lander/game_states/play'
